b2b9301664c5357e59a2a34fd66fdca73591d6fe
Christie Renaud <mail@christierenaud.com>
2022-03-11 18:17:50 -0500

    Header and React Router Added

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+            isNavOpen: false
+        };
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    render() {
+        return (
+            <Navbar dark sticky="top" expand="sm">
+                <div className="container">
+                    <NavbarBrand className="ml-auto" href="/"><img src="/assets/images/white_silhouette.png" height="32" width="32" alt="white cat silhouette" /> Calico Cafe</NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNav} />
+                    <Collapse isOpen={this.state.isNavOpen} navbar  className="justify-content-end">
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/home"> Home </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/visit"> Visit </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/adopt"> Adopt </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/events"> Events </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </div>
+            </Navbar>
+        );
+    }
+}
+
+export default Header;
